2b06582a61bb0702d618ca631e6829b9cd5d3179
Hamza <hamza@tilak.io>
2022-10-13 15:51:38 +0200

    removed debug mode to kill process when quitting, issue #11

--- api/server.py
+++ api/server.py
@@ -136,4 +136,4 @@ def disconnected():
     print("client has disconnected " + request.sid)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='127.0.0.1', port=5000)
+    socketio.run(app, host='127.0.0.1', port=5000)
