66d9aa18f2e10be77a6e829010405914d754248c
Hamza ZOGHMAR <hamza@tilak.io>
2022-11-18 17:01:52 +0100

    convert time to seconds for csv parser

--- api/csvparser.py
+++ api/csvparser.py
@@ -12,12 +12,13 @@ class CSVParser(Parser):
 
     def parse(self,filename):
         csv = pd.read_csv(filename)
-        time_delta = pd.to_datetime(csv['timestamp'])
+        start_time = pd.to_datetime(csv['timestamp'][0])
+        time_delta = (pd.to_datetime(csv['timestamp']) - start_time)
         seconds = time_delta / np.timedelta64(1, 's')
         micro_seconds = time_delta / np.timedelta64(1, 'us')
         csv['timestamp'] = micro_seconds
         csv['timestamp_tiplot'] = seconds
-        self.datadict = {"data": csv}
+        self.datadict = {"data": csv} 
         return [self.datadict, self.entities]
 
     def initDefaultEntities(self):
