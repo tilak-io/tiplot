81ba71a73a8635a217117a223eb94b34099c4a06
Hamza ZOGHMAR <hamza@tilak.io>
2022-11-20 02:09:55 +0100

    setting up PyInstaller to build the backend on windows

--- package_python.js
+++ package_python.js
@@ -1,28 +1,24 @@
 const path = require("path");
+const isWin = process.platform === "win32";
+
+var add_data;
+if (isWin) add_data = "--add-data api;api";
+else add_data = "--add-data api:api";
 
 const spawn = require("child_process").spawn,
   ls = spawn(
     "python3 -m PyInstaller",
-    [
-      "-w",
-      "--onefile",
-      "--distpath backend",
-      "--add-data api;api",
-      "api/server.py",
-    ],
+    ["-w", "--onefile", "--distpath backend", add_data, "api/server.py"],
     {
       shell: true,
     }
   );
 
-ls.stdout.on("data", function (data) {
+ls.stderr.on("data", function (data) {
   // stream output of build process
   console.log(data.toString());
 });
 
-ls.stderr.on("data", function (data) {
-  console.log("Packaging error: " + data.toString());
-});
 ls.on("exit", function (code) {
   console.log("child process exited with code " + code.toString());
 });
