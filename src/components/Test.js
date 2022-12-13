6f278d5e1a107290c1883becc4dbb9efeb53aba2
Hamza ZOGHMAR <hamza@tilak.io>
2022-12-13 17:51:03 +0100

    Resize new plots to fit screen

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,27 +1,67 @@
 import "../../node_modules/react-grid-layout/css/styles.css";
-import "../../node_modules/react-resizable/css/styles.css";
-import GridLayout from "react-grid-layout";
+import { useState, useEffect } from "react";
+import TopBar from "./TopBar";
+import { WidthProvider, Responsive } from "react-grid-layout";
+import Graph from "./Graph";
+
+import { v4 as uuid } from "uuid";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 function Test() {
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2 },
-    { i: "b", x: 1, y: 0, w: 3, h: 2 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 },
-  ];
+  const [graphs, setGraphs] = useState([]);
+  const [rowHeight, setRowHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    fitGraphsToScreen();
+  }, [graphs]);
+
+  const addGraph = () => {
+    const graph = { id: uuid() };
+    setGraphs([...graphs, graph]);
+  };
+
+  const removeGraph = (id) => {
+    const g = graphs.filter((e) => e.id != id);
+    setGraphs(g);
+  };
+
+  const fitGraphsToScreen = () => {
+    const containers = document.getElementsByClassName("plot-yt");
+    const multiselects = document.getElementsByClassName("multiselect");
+    var additionalHeight = 0; // buttons + navbar height
+    for (var i = 0; i < multiselects.length; i++)
+      additionalHeight += multiselects[i].clientHeight;
+    const plotHeight =
+      (window.innerHeight - additionalHeight) / containers.length;
+    for (var j = 0; j < containers.length; j++)
+      containers[j].style.height = plotHeight + "px";
+    setRowHeight(plotHeight);
+  };
+
+  const updateKeys = () => {};
   return (
-    <GridLayout className="layout" layout={layout} cols={1} width={100}>
-      <div key="a" style={{ backgroundColor: "red" }}>
-        a
-      </div>
-
-      <div key="b" style={{ backgroundColor: "red" }}>
-        a
-      </div>
-
-      <div key="c" style={{ backgroundColor: "red" }}>
-        a
-      </div>
-    </GridLayout>
+    <>
+      <TopBar addX={addGraph} />
+      <ResponsiveGridLayout
+        isResizable={false}
+        margin={[0, 0]}
+        rowHeight={rowHeight}
+        className="layout"
+        cols={{ lg: 1, md: 1, sm: 1, xs: 1, xxs: 1 }}
+        draggableHandle=".plot-options"
+      >
+        {graphs.map((g) => (
+          <div key={g.id} style={{ backgroundColor: "red", height: 100 }}>
+            <Graph
+              id={g.id}
+              updateKeys={updateKeys}
+              removeGraph={removeGraph}
+            />
+          </div>
+        ))}
+      </ResponsiveGridLayout>
+    </>
   );
 }
 
