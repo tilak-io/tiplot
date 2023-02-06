6b51ad110abd7f9858a37815476973ca1be10be5
Hamza ZOGHMAR <hamza@tilak.io>
2023-02-06 16:26:05 +0100

    fix the xaxis when the sync plot is zoomed in

--- src/views/SyncTimestamp.js
+++ src/views/SyncTimestamp.js
@@ -10,14 +10,13 @@ function SyncTimestamp() {
 
   const [mainOptions, setMainOptions] = useState([]);
   const [mainData, setMainData] = useState({});
-
   const [extraOptions, setExtraOptions] = useState([]);
   const [extraData, setExtraData] = useState({});
   const [shiftedData, setShiftedData] = useState({});
-
   const [data, setData] = useState([]);
-
   const [delta, setDelta] = useState(0);
+  const [xaxis, setXAxis] = useState(null);
+  const [zoomed, setZoomed] = useState(false);
 
   useEffect(() => {
     getOptions();
@@ -30,6 +29,7 @@ function SyncTimestamp() {
 
   useEffect(() => {
     shiftTimestamp(delta);
+    // eslint-disable-next-line
   }, [delta]);
 
   const getOptions = async () => {
@@ -42,6 +42,7 @@ function SyncTimestamp() {
 
   const getMainData = async (field) => {
     const d = await plotData.getData(field, false);
+    setXAxis([d.x[0], d.x[d.x.length - 1]]);
     setMainData(d);
   };
 
@@ -49,14 +50,43 @@ function SyncTimestamp() {
     const d = await plotData.getData(field, true);
     setExtraData(d);
     setShiftedData(d);
+    updateXAxis();
   };
 
   const shiftTimestamp = (_delta) => {
+    console.log(xaxis);
     const dt = parseFloat(_delta);
     const ed = Object.assign({}, extraData);
     if ("x" in ed) {
       ed.x = ed.x.map((t) => t + dt);
       setShiftedData(ed);
+    }
+    updateXAxis();
+  };
+
+  const updateXAxis = () => {
+    if (zoomed) return;
+    if ("x" in mainData && "x" in shiftedData) {
+      const main_x = mainData.x;
+      const shifted_x = shiftedData.x;
+
+      const x0 = Math.min(main_x[0], shifted_x[0]);
+      const x1 = Math.max(
+        main_x[main_x.length - 1],
+        shifted_x[shifted_x.length - 1]
+      );
+      setXAxis([x0, x1]);
+    }
+  };
+
+  const handleRelayout = (event) => {
+    if (
+      event["xaxis.autorange"] !== undefined &&
+      event["yaxis.autorange"] !== undefined
+    ) {
+      setZoomed(false);
+    } else {
+      setZoomed(true);
     }
   };
 
@@ -98,6 +128,7 @@ function SyncTimestamp() {
         <Plot
           data={data}
           style={{ width: "100%", height: "100%" }}
+          onRelayout={handleRelayout}
           layout={{
             autoresize: true,
             showlegend: true,
@@ -105,6 +136,9 @@ function SyncTimestamp() {
               x: 1,
               xanchor: "right",
               y: 1,
+            },
+            xaxis: {
+              range: xaxis,
             },
           }}
           config={{
