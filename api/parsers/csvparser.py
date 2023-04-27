b37d87e7e966ce4fde30d3da66b8d6adf8b55208
Pierre Tilak <pierre.tilak@gmail.com>
2023-04-27 08:53:53 +0200

    WIP Testing Preprocessor
    Trying to figure a way to load at runtime a file that
    has the same name as the log but with a python extension
    which contains a single "preprocessor" function that will be c
    called by the api just before the loading.
    The file should be in /home/USER/Documents/tiplot/logs
    Problem : The .py file should not show up in the list, maybe they should
    get stored somewhere else (like /home/USER/Documents/tiplots/preprocessors) ?

--- api/parsers/csvparser.py
+++ api/parsers/csvparser.py
@@ -2,6 +2,9 @@ from pandas import read_csv, to_datetime
 import numpy as np
 from cesium_entity import CesiumEntity
 from .parser import Parser
+import sys
+import importlib
+import os
 
 class CSVParser(Parser):
     def __init__(self):
@@ -12,6 +15,16 @@ class CSVParser(Parser):
 
     def parse(self,filename):
         csv = read_csv(filename)
+
+        prepfilename = filename[:-3]+'py'
+        print((prepfilename))
+        print(os.path.exists(prepfilename))
+        if(os.path.exists(prepfilename)):
+           print("Preprocessor Exist")
+           sys.path.append(os.path.abspath(prepfilename))
+           module = importlib.import_module(os.path.basename(prepfilename).split('.')[0])
+           csv = module.preprocess(csv)
+
         start_time = to_datetime(csv['timestamp'][0])
         time_delta = (to_datetime(csv['timestamp']) - start_time)
         seconds = time_delta / np.timedelta64(1, 's')
